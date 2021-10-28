bc773aeff59e88e84b315efeb78ad185b579c522
Jon McManus <due@wxwhatever.com>
2021-10-28 18:19:55 +1000

    Horse shit.

---
+++ src/main/java/noobanidus/mods/souljar/events/HorseShitHandler.java
@@ -0,0 +1,48 @@
+package noobanidus.mods.souljar.events;
+
+import com.google.common.collect.Sets;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import noobanidus.mods.souljar.SoulJar;
+import noobanidus.mods.souljar.init.ModItems;
+
+import java.util.Set;
+
+@Mod.EventBusSubscriber(modid = SoulJar.MODID)
+public class HorseShitHandler {
+  private static final Set<EntityType<?>> HORSES = Sets.newHashSet(EntityType.HORSE, EntityType.DONKEY, EntityType.MULE, EntityType.ZOMBIE_HORSE, EntityType.SKELETON_HORSE, EntityType.VILLAGER, EntityType.LLAMA, EntityType.TRADER_LLAMA, EntityType.WANDERING_TRADER, EntityType.PIG, EntityType.STRIDER);
+
+  @SubscribeEvent
+  public static void onRightClick(PlayerInteractEvent.EntityInteract event) {
+    if (event.getTarget().getEntityWorld().isRemote) {
+      return;
+    }
+
+    ItemStack stack;
+    Hand hand;
+
+    if (ModItems.SOUL_JAR.get().equals(event.getPlayer().getHeldItemMainhand().getItem())) {
+      hand = Hand.MAIN_HAND;
+      stack = event.getPlayer().getHeldItemMainhand();
+    } else if (ModItems.SOUL_JAR.get().equals(event.getPlayer().getHeldItemOffhand().getItem())) {
+      hand = Hand.OFF_HAND;
+      stack = event.getPlayer().getHeldItemOffhand();
+    } else {
+      return;
+    }
+
+    if (!HORSES.contains(event.getTarget().getType())) {
+      return;
+    }
+
+    ActionResultType result = ModItems.SOUL_JAR.get().itemInteractionForEntity(stack, event.getPlayer(), (LivingEntity) event.getTarget(), hand);
+    event.setCanceled(true);
+    event.setCancellationResult(result);
+  }
+}
